87f0d9b8e3b5e21f3f4d119d0878ddf8ef96a61b
Feng Xiong <fengxiong34@gmail.com>
2020-09-03 16:46:39 -0700

    added Switch

--- App.tsx
+++ App.tsx
@@ -1,65 +1,28 @@
 import React, { useState } from 'react';
-import { Button, Text, StyleSheet, StatusBar, View } from 'react-native';
-
-import Constants from 'expo-constants';
-
+import Header from './components/common/header';
+import { Switch, View, StyleSheet } from 'react-native';
 const App = () => {
-  const styleTypes: any = ['default', 'dark-content', 'light-content'];
-  const [visibleStatusBar, setVisibleStatusBar] = useState(false);
-  const [styleStatusBar, setStyleStatusBar] = useState(styleTypes[0]);
-
-  const changeVisibilityStatusBar = () => {
-    setVisibleStatusBar(!visibleStatusBar);
-  };
-
-  const changeStyleStatusBar = () => {
-    const styleId = styleTypes.indexOf(styleStatusBar) + 1;
-
-    if (styleId === styleTypes.length) {
-      return setStyleStatusBar(styleTypes[0]);
-    }
-    return setStyleStatusBar(styleTypes[styleId]);
-  };
+  const [isEnabled, setIsEnabled] = useState(true);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
-    <View style={{ ...styles.container, backgroundColor: 'yellow' }}>
-      <View>
-        <Text style={styles.textStyle}>StatusBar Style: {styleStatusBar}</Text>
-        <Text style={styles.textStyle}>
-          StatusBar Visibility: {!visibleStatusBar ? 'Visible' : 'Hidden'}
-        </Text>
-      </View>
-      <StatusBar backgroundColor='blue' barStyle={styleStatusBar} />
-      <StatusBar hidden={visibleStatusBar} />
-      <View style={styles.buttonContainer}>
-        <Button
-          title='Toggle StatusBar'
-          onPress={() => changeVisibilityStatusBar()}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title='Change StatusBar Style'
-          onPress={() => changeStyleStatusBar()}
-        />
-      </View>
+    <View style={styles.container}>
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor='#3e3e3e'
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 300,
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    padding: 8,
-  },
-  buttonContainer: {
-    padding: 10,
-  },
-  textStyle: {
-    textAlign: 'center',
   },
 });
 
